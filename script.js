9392e3affd34e3d21db8263c2266e72e52e2837e
geovannasousa33-boop <geovanna.sousa33@aluno.educa.go.gov.br>
2025-11-14 13:53:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Animação simples ao carregar a página
-window.onload = function() {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        section.style.opacity = 0;
-        section.style.transition = 'opacity 1s ease-in';
-        setTimeout(() => {
-            section.style.opacity = 1;
-        }, 300);
-    });
-};
+
